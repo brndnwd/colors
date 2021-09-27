743856d01a7675308e5c611e8ef7b4c8cd89e68c
bdwood <bdwood@pop-os.localdomain>
2021-09-27 00:24:56 -0400

    .ipynb to .py

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # colors
 built to select top n most common colors in an image
 
-using:
+## usage
+drop images to analyzed into directory ./images (currently set up for .jpg files)
+requires the file "benchmark_colors.csv" to provide a set of possible colors for output
+run in shell: >python colors.py
+program will read all images in ./images and create csv file results.csv
+showing top n most frequent colors for each image
+program defaults to n=3, which may be changed via optional argument
+ex: >python colors.py 4 
+
+requirements:
   python 3.8.0
-  pandas 0.25.3
-  pillow 6.2.1
+  numpy 1.21.2
+  pandas 1.3.3
+  Pillow 8.3.2
---
+++ colors.py
@@ -0,0 +1,80 @@
+import os
+import sys
+from PIL import Image
+import pandas as pd
+import numpy as np
+
+
+def top_n_colors(image, color_array, color_name_array,n):
+    '''
+        image: jpg image
+        color_array: np.array of the colors we want to shift to
+        color_name_array: np.array containing names corresponding to the color_array
+        n: number of dominant colors we want to find
+        function calculates the distance from each of the image's pixels
+        to each discrete color we want to include in our analysis
+        then takes the closest color to each pixel and returns the top
+        n of those closest colors
+    '''
+    # Open image with PIL's Image class .open() method
+    # convert to np.array using getdata() method which 
+    # returns r,g,b for each pixel 
+    image_array = np.array(list(Image.open(image).getdata()))
+    size = image_array.shape[0]
+
+    # loop through colors and build distance array which is a 
+    # stack of each pixels distance to each color
+    for i in range(len(color_array)):
+        if i == 0:
+            distance_array = ((image_array - color_array[i]) ** 2).sum(axis=1)
+        else:
+            distance_array = np.vstack((distance_array, ((image_array - color_array[i]) ** 2).sum(axis=1)))
+
+    # indexes of min distance for each pixel where the index
+    # corresponds to the color with that min distance
+    mindexes = np.argmin(distance_array, axis=0)
+
+    # index into the color_names_array with the mindexes
+    # then grab the top n unique colors and counts
+    unique_counts = list(np.unique(color_name_array[mindexes], return_counts=True))
+
+    # unfortunate that is not sorted so we converted to list of np.arrays
+    # and loop through taking the next highest frequency color and deleting
+    colors = []
+    for _ in range(n):
+        # if image has less colors than we asked for
+        if len(unique_counts[0]) == 0:
+            break
+
+        max_idx = np.argmax(unique_counts[1])
+        # if next most frequent color makes up less than 10%
+        if unique_counts[1][max_idx] / size < 0.1:
+            break
+
+        colors.append(unique_counts[0][max_idx])
+        unique_counts[0] = np.delete(unique_counts[0], max_idx)
+        unique_counts[1] = np.delete(unique_counts[1], max_idx)
+
+    # if we broke the loop early
+    if len(colors) < n:
+        # add None's to fill the void
+        colors += [None for _ in range(n-len(colors))]
+
+    return colors
+
+benchmarks = pd.read_csv('benchmark_colors.csv')  # benchmark color file with rgb values
+color_array = benchmarks[['r', 'g', 'b']].to_numpy()  # convert to np array for speed
+color_name_array = benchmarks['name'].to_numpy()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        n = 3
+    else:
+        n = int(sys.argv[1])
+    cols = ['filename'] + [f'color_rank{i+1}' for i in range(n)]
+    df = pd.DataFrame(columns=cols)
+    for file in os.listdir('./images/'):
+        tnc = top_n_colors('./images/' + f'{file}', color_array, color_name_array,n)
+        df = df.append(pd.DataFrame([[file] + tnc], columns=cols))
+    df.to_csv('results.csv', index=False)
+
